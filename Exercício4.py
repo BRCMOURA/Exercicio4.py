b1ad5120a04019e107cfdbc3def1b5fa52e5a067
BRCMOURA <brcmoura@gmail.com>
2024-10-04 20:56:23 -0300

    Add files via upload

---
+++ Exercício4.py
@@ -0,0 +1,8 @@
+#Divisão de dois números
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+divisao = (n1 / n2)
+
+print("A divisão dos dois números é: ", divisao)
